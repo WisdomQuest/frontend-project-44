35cb9acae614071bf55e8ad8519fcd874e26349b
Pavel Sofronov <sofronovpv89@mail.ru>
2024-11-14 12:52:15 +0400

    8. update progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -4,16 +4,18 @@ import randomNumber from '../utilRandomNumber.js';
 const descriptionGame = 'What number is missing in the progression?';
 
 const actionGame = () => {
-  const progression = [];
+  let randomNum = randomNumber();
+  const arithmeticProgression = randomNumber(5) + 1;
+  const progression = [randomNum];
   for (let i = 0; i < 10; i += 1) {
-    progression.push(randomNumber());
+    randomNum += arithmeticProgression;
+    progression.push(randomNum);
   }
-  const arithmeticProgression = progression.sort((a, b) => a - b);
   const hiddenNum = randomNumber(10);
-  const correctAnswer = arithmeticProgression[hiddenNum];
-  arithmeticProgression[hiddenNum] = '..';
+  const correctAnswer = progression[hiddenNum];
+  progression[hiddenNum] = '..';
   let question = '';
-  arithmeticProgression.forEach((element) => {
+  progression.forEach((element) => {
     question += `${element} `;
   });
   return [question, String(correctAnswer)];
